82dec4408e2fc603eb41b84fc4b0f963c69a0e4b
Wenter <uuooooq@gmail.com>
2020-04-09 20:00:44 +0800

    删除无用代码

--- notex/src/App.js
+++ notex/src/App.js
@@ -1,42 +1,17 @@
 import React from 'react';
-//import logo from './logo.svg';
 import './App.css';
-//import ScrollList from './scrollList'
-import { Input, Affix, Button, List, Divider, Radio } from 'antd';
+import { Affix, Button, List, Divider, Radio } from 'antd';
 import { Layout } from 'antd';
 import TextareaAutosize from 'react-textarea-autosize';
-//import { MessageOutlined, LikeOutlined, StarOutlined } from '@ant-design/icons';
-//import reqwest from 'reqwest';
-import { GET } from 'react-axios'
-import axios from 'axios'
 import 'antd/dist/antd.css';
 const { Content } = Layout;
-const { Search } = Input;
 
-const httpPre = 'http://'+window.location.host
-
-// var listData = [];
-// for (let i = 0; i < 23; i++) {
-//   listData.push({
-//     href: 'http://ant.design',
-//     title: `ant design part ${i}`,
-//     avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-//     description:
-//       'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-//     content:
-//       'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-//   });
-// }  
+//const httpPre = 'http://'+window.location.host //http://192.168.1.103:8080
+const httpPre =  'http://192.168.1.103:8080'
+  
 
 var listData = [];
-// for (let i = 0; i < 23; i++) {
-//   listData.push({
-//     key: 'http://ant.design',
-//     value: '这是一段记录',
-//     createTime: '',
-//     extCategory:{'type':'text'},
-//   });
-// } 
+
 
 var storage = window.localStorage
 if (storage) {
@@ -72,15 +47,6 @@ export default class App extends React.Component {
   componentWillMount() {
     this.fetchData()
     document.addEventListener("keydown", this.onKeyDown)
-    // if (storage) {
-    //   var tmpListData = JSON.parse(storage.getItem('personalStorageNew'))
-    //   if (tmpListData) {
-    //     this.setState({
-    //       myData: tmpListData,
-    //     })
-    //   }
-    //   //console.log('from will mount' + tmpListData)
-    // }
 
   }
 
@@ -122,16 +88,6 @@ export default class App extends React.Component {
 
   onSave(e) {
 
-    // if (storage) {
-    //   var tmpListData = JSON.parse(storage.getItem('personalStorageNew'))
-    // var timestamp = (new Date()).valueOf();
-    // this.state.myData.unshift({
-    //   key: `${timestamp}`,
-    //   value: this.state.inputValue,
-    //   createTime: timestamp,
-    //   extCategory: { 'type': 'text' },
-    // })
-    //   storage.setItem('personalStorageNew', JSON.stringify(tmpListData))
     this.sendToServer(this.state.inputValue)
     //this.state.myData.
     if (tmpListData) {
@@ -139,12 +95,6 @@ export default class App extends React.Component {
         inputValue: '',
       })
     }
-    //this.fetchData()
-
-
-    //}
-
-    //console.log(this.state.inputValue);
   }
 
   fetchData() {
@@ -162,79 +112,13 @@ export default class App extends React.Component {
         this.setState({ requestFailed: true });
       });
 
-    // fetch('http://192.168.1.6:8080/hello?start=0&end=100',{
-    //   method:'GET',
-    //   // header:{
-    //   //   'Control-Type':'application/json'
-    //   // },
-    //   mode: 'no-cors',
-    // }).then(res => {
-    //   //var tmp = res.body()
-    //   //console.log(res.json())
-    //   if(res.ok){
-    //     res.json().then((data) => {
-    //       console.log('can you see'+data)
-    //     })
-    //   }else{
-    //     console.log(res.status)
-    //   }
-    // }).catch(err =>{
-    //   console.log('错误'+err)
-    // })
-    // var a = {}
-    // reqwest({
-    //     url:"http://192.168.1.6:8080/hello?start=0&end=100",
-    //     type:"json",
-    //     method:"get",
-    //     mode: 'no-cors',
-    //     header:{
-    //       'Access-Control-Allow-Origin':'*'
-    //     },
-    //     data:{tag:"life"},
-    //     success:function (resp) {
-    //         a = resp
-    //         console.log(resp)
-    //     }
-    // })
-
-    // axios.get('http://192.168.1.6:8080/hello?start=0&end=100').then(function (response) {
-    //   console.log(response.data)
-
-    // })
-    // .catch(function (error) {
-    //   console.log(error);
-    // })
-
   }
 
   sendToServer(valueStr) {
-    // fetch('http://192.168.1.6:8080',{
-    //   method:'POST',
-    //   headers:{
-    //     'Content-type':'application/x-www-form-urlencoded'
-    //   },
-    //   body:JSON.stringify({
-    //     'value':valueStr,
-    //   })
-    // })
-
-    // let formdata = new FormData();
-    // formdata.append("name","admin");
-    // fetch("http://192.168.1.6:8080",{
-    //     method:"POST",
-    //     headers:{
-    //       'Content-Type':'application/x-www-form-urlencoded'
-    //     },
-    //     body:formdata
-    // }).then(function(response){
-    //     console.log(response);
-    // })
     let url = httpPre//"http://192.168.1.6:8080";
     let formData = new FormData();
-    //formData.append('c','login');
+
     formData.append('value', valueStr);
-    //formData.append('password', valueStr);
-    //formData.append('client', 'android');
 
     fetch(url, {
       method: 'post',
@@ -271,15 +155,6 @@ export default class App extends React.Component {
             <Affix offsetTop={0} >
               <div style={{ display: 'flex', width: '100%', flexDirection: 'column' }}>
                 <div style={{ display: 'flex', width: '100%', backgroundColor: 'white', flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'baseline' }}>
-                  {/* <Button>添加照片，视频等文件</Button>
-                <Button style={{ flexGrow: '1' }}>代码</Button> */}
-                  <div style={{ display: 'flex', width: '100%', backgroundColor: 'white', flexDirection: 'row', justifyContent: 'flex-start' }}>
-                    <Radio.Group onChange={this.onChange.bind(this)} value={this.state.value}>
-                      <Radio value={1}>文本</Radio>
-                      <Radio value={2}>文件</Radio>
-              <Radio value={3}>代码{httpPre}</Radio>
-                    </Radio.Group>
-                  </div>
                   <Button>取消(ctrl+c)</Button>
                   <Button onClick={this.onSave.bind(this)} >保存(ctrl+s)</Button>
                 </div>
@@ -322,14 +197,3 @@ export default class App extends React.Component {
     );
   }
 }
-
-
-{/* <Affix offsetTop={0}>
-<div style={{ width: '100%' }}>
-  <Search
-    placeholder="input search text"
-    onSearch={value => console.log(value)}
-    style={{ width: '100%' }}
-  />
-</div>
-</Affix> */}
