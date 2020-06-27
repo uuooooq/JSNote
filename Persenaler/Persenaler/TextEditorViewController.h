43fc01b553a14685960654c16a4fe480dcbd6688
Wenter <uuooooq@gmail.com>
2020-06-28 07:06:54 +0800

    建立新的文字编辑功能

---
+++ Persenaler/Persenaler/TextEditorViewController.h
@@ -0,0 +1,17 @@
+//
+//  TextEditorViewController.h
+//  Persenaler
+//
+//  Created by zhu dongwei on 2020/6/27.
+//  Copyright © 2020 zhu dongwei. All rights reserved.
+//
+
+#import <ZSSRichTextEditor/ZSSRichTextEditor.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TextEditorViewController : ZSSRichTextEditor
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Persenaler/Persenaler/TextEditorViewController.m
@@ -0,0 +1,32 @@
+//
+//  TextEditorViewController.m
+//  Persenaler
+//
+//  Created by zhu dongwei on 2020/6/27.
+//  Copyright © 2020 zhu dongwei. All rights reserved.
+//
+
+#import "TextEditorViewController.h"
+
+@interface TextEditorViewController ()
+
+@end
+
+@implementation TextEditorViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
